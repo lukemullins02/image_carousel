bb5fd541722785e181c055cd9a011f6cec195292
= <lukejmullins@gmail.com>
2025-12-29 12:08:02 -0500

    Add previous and next button

--- carousel.js
+++ carousel.js
@@ -1,7 +1,10 @@
 let i = 0;
-let id = 1;
+let id = 0;
 
 const images = document.querySelectorAll(".image");
+const nextBtn = document.querySelector(".next");
+const prevBtn = document.querySelector(".previous");
+const container = document.querySelector(".container");
 
 images.forEach((image) => {
   image.dataset.id = i;
@@ -9,28 +12,50 @@ images.forEach((image) => {
 });
 
 const napoleon = document.querySelector(`img[data-id="0"]`);
+napoleon.classList.toggle("invisible");
 
-function switchPic() {
-  let prev;
-  if (id === 0) {
-    prev = images.length - 1;
-  } else {
-    prev = id - 1;
-  }
-
-  const show_image = document.querySelector(`img[data-id="${id}"]`);
-  const hide_image = document.querySelector(`img[data-id="${prev}"]`);
-
-  hide_image.classList.toggle("invisible");
-  show_image.classList.toggle("invisible");
-
+function next() {
+  let prev = id;
   if (id === images.length - 1) {
     id = 0;
   } else {
     id++;
   }
+
+  const show_image = document.querySelector(`img[data-id="${id}"]`);
+  const hide_image = document.querySelector(`img[data-id="${prev}"]`);
+
+  console.log(show_image);
+  console.log(hide_image);
+
+  hide_image.classList.toggle("invisible");
+  show_image.classList.toggle("invisible");
 }
 
-napoleon.classList.toggle("invisible");
+function previous() {
+  let prev = id;
+  if (id === 0) {
+    id = images.length - 1;
+  } else {
+    id--;
+  }
 
-setInterval(() => switchPic(), 5000);
+  const show_image = document.querySelector(`img[data-id="${id}"]`);
+  const hide_image = document.querySelector(`img[data-id="${prev}"]`);
+
+  console.log(show_image);
+  console.log(hide_image);
+
+  hide_image.classList.toggle("invisible");
+  show_image.classList.toggle("invisible");
+}
+
+nextBtn.addEventListener("click", () => {
+  next();
+});
+
+prevBtn.addEventListener("click", () => {
+  previous();
+});
+
+// setInterval(() => switchPic(), 5000);
